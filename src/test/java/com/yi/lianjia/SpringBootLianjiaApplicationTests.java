2da031d6737a062d26d61700a224f589e71b1d14
chenhang <18730051277@163.com>
2020-04-22 11:46:39 +0800

    修改pom文件

--- src/test/java/com/yi/lianjia/SpringBootLianjiaApplicationTests.java
+++ src/test/java/com/yi/lianjia/SpringBootLianjiaApplicationTests.java
@@ -15,7 +15,7 @@ class SpringBootLianjiaApplicationTests {
      */
     @Test
     void contextLoads() {
-        for (int i = 1; i <= 60; i++) {
+        for (int i = 61; i <= 100; i++) {
             lianjiaInfoService.cronTask(i);
             System.out.println("---------------------------------当前爬取到了第"+i+"页--------------------------");
         }
